9eee4622f06b7970044052b59e21d4dc7f1b4b06
jayaseelan4215 <jayaseelan4215@gmail.com>
2025-12-18 12:00:23 +0530

    nsremoved

--- src/hooks/useWindowSize.jsx
+++ src/hooks/useWindowSize.jsx
@@ -13,10 +13,10 @@ function useWindowSize() {
         height: window.innerHeight,
       });
     }
-    
+
     window.addEventListener('resize', handleResize);
     handleResize();
-    
+
     return () => window.removeEventListener('resize', handleResize);
   }, []);
 
